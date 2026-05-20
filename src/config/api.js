eeae17ff56385a170442542ea1402c5473bc452e
santicerio261 <santi25cerioo@gmail.com>
2026-05-20 19:06:48 -0300

    api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // Para cambiar de servidor, modificar solo SERVER_IP.
-export const SERVER_IP = "172.16.4.48";
-
+// export const SERVER_IP = "172.16.4.48";   //desmarcar esta linea cuando se trabaja en la escuela
+export const SERVER_IP = "186.139.84.209";  //desmarcar esta linea cuando se trabaja fuera de la escuela 
 export const API_PORT = "3000";
 export const AI_PORT = "5000";
 
