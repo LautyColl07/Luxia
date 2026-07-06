6b4b9ab13d52d36d9c17d91cfd2e3ff9eb18301f
unknown <tig2@itisjt.edu>
2026-07-06 09:11:43 -0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // Para cambiar de servidor, modificar solo SERVER_IP.
-//export const SERVER_IP = "172.16.4.48";   //desmarcar esta linea cuando se trabaja en la escuela
+export const SERVER_IP = "172.16.4.48";   //desmarcar esta linea cuando se trabaja en la escuela
 //export const SERVER_IP = "186.139.84.209";  //desmarcar esta linea cuando se trabaja fuera de la escuela
-export const SERVER_IP = "192.168.0.50";
+
 export const API_PORT = "3000";
 export const AI_PORT = "5000";
 
